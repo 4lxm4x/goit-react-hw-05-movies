59efa93e5578aa49ccb202033a1f069a9b74204e
Oleksandr Maksymenko <machinery69@gmail.com>
2024-03-04 19:45:24 +0200

    added react.lazy and suspense

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,19 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
-import Layout from './Layout/Layout';
-import Home from 'pages/Home';
-import Movies from 'pages/Movies';
-import MovieDetails from 'pages/MovieDetails';
-import Cast from './Cast/Cast';
-import Reviews from './Reviews/Reviews';
+import { lazy, Suspense } from 'react';
+import ReactLoading from 'react-loading';
+// import Layout from './Layout/Layout';
+// import Home from 'pages/Home';
+// import Movies from 'pages/Movies';
+// import MovieDetails from 'pages/MovieDetails';
+// import Cast from './Cast/Cast';
+// import Reviews from './Reviews/Reviews';
+
+const Home = lazy(() => import('pages/Home'));
+const Movies = lazy(() => import('pages/Movies'));
+const MovieDetails = lazy(() => import('pages/MovieDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+const Layout = lazy(() => import('./Layout/Layout'));
 
 export default function App() {
   const location = useLocation();
@@ -12,7 +21,13 @@ export default function App() {
   console.log(location.pathname);
 
   return (
-    <div>
+    <Suspense
+      fallback={
+        <div>
+          <ReactLoading type="spin" color="red" />
+        </div>
+      }
+    >
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -27,6 +42,6 @@ export default function App() {
           <Route path="*" element={<Home />} />
         </Route>
       </Routes>
-    </div>
+    </Suspense>
   );
 }
