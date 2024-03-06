170c0dca7d7c8fa85383732f890d36a640f68be8
4lxm4x <machinery69@gmail.com>
2024-03-06 11:54:19 +0000

    Deploying to gh-pages from @ 4lxm4x/goit-react-hw-05-movies@d3bfefdaf3cca1a0117065da3bdbc79fd3f755ee 🚀

--- static/js/351.24f60fdf.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[351],{351:function(e,s,i){i.r(s),i.d(s,{default:function(){return c}});var n=i(87),t=i(689),r=i(184);function c(){return(0,r.jsxs)("div",{children:[(0,r.jsxs)("nav",{children:[(0,r.jsx)(n.OL,{to:"/",children:"Home"}),(0,r.jsx)(n.OL,{to:"/movies",children:"Movies"})]}),(0,r.jsx)(t.j3,{})]})}}}]);
-//# sourceMappingURL=351.24f60fdf.chunk.js.map
